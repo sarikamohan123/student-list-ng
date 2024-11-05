1ae501754a0ba9efd5b14db0e89f4a2cbfd66e28
sarika srivastava <sarikasrivastava2005@gmail.com>
2024-11-04 22:02:54 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.comonent.html',
+  styles: ['h1{color:red;}']
 })
 export class AppComponent {
   title = 'student-list';
